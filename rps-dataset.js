f57bfda3ab658f53f6ba2f51058e758777ac8db6
DJ <dweepjoshipuracar@gmail.com>
2021-08-30 11:26:58 +0530

    Just some minor editing

--- rps-dataset.js
+++ rps-dataset.js
@@ -20,11 +20,13 @@ class RPSDataset {
     for (var i = 0; i < this.labels.length; i++) {
       if (this.ys == null) {
         this.ys = tf.keep(tf.tidy(
-            () => {return tf.oneHot(
+            () => {
+              return tf.oneHot(
                 tf.tensor1d([this.labels[i]]).toInt(), numClasses)}));
       } else {
         const y = tf.tidy(
-            () => {return tf.oneHot(
+            () => {
+              return tf.oneHot(
                 tf.tensor1d([this.labels[i]]).toInt(), numClasses)});
         const oldY = this.ys;
         this.ys = tf.keep(oldY.concat(y, 0));
